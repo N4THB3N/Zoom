4914018cd9d33b2304d7e8eabd8767f1af98c635
NATHANBASIS <59455965+NATHANBASIS@users.noreply.github.com>
2020-04-28 17:06:54 -0600

    Creating zoom.js

--- zoomverify/zoom.js
+++
@@ -1,9 +0,0 @@
-var path = require('path');
-const express = require('express');
-const router2 = express.Router();
-
-router2.get('/zoomverify/verifyzoom.html', (req, res) => {
-    res.sendFile(path.join(__dirname+'/verifyzoom.html'));
-})
-
-module.exports = router2
